69f3001e25170b78461992b5162eb847f94cee8f
VictorYarosh <77171768+VictorYarosh@users.noreply.github.com>
2022-10-17 23:25:22 +0300

    global styled fix

--- src/components/ui/global.styles.ts
+++ src/components/ui/global.styles.ts
@@ -9,6 +9,11 @@ const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+  background-color {
+    linear-gradient(217deg,#6447b5,rgba(255,0,0,0) 70.71%), 
+    linear-gradient(127deg,#82e1ff,rgba(0,255,0,0) 70.71%), 
+    linear-gradient(336deg,#6447b5,rgba(0,0,255,0) 70.71%)
+  }
   code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
     monospace;
