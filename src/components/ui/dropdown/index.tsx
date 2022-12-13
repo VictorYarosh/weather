2099e3458b1c1f1369dc7b67ae0c52b5608f5701
VictorYarosh <77171768+VictorYarosh@users.noreply.github.com>
2022-12-13 15:24:28 +0200

    fix dropdown default

--- src/components/ui/dropdown/index.tsx
+++ src/components/ui/dropdown/index.tsx
@@ -18,7 +18,7 @@ const DropdownMenu = () => {
 
   return (
     <DropdownWrapper>
-      {dropdownActive ? (
+      {!dropdownActive ? (
         <Dropdown>
           <DropdownList onClick={handleToggleDropdown}>
             <DropdownListLi></DropdownListLi>
