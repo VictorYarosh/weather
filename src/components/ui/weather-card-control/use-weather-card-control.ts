1224b72044b4ae94ec9841d1f5f7e030f45373d1
VictorYarosh <77171768+VictorYarosh@users.noreply.github.com>
2023-01-02 23:30:42 +0200

    wip use-weather-card-control

--- src/components/ui/weather-card-control/use-weather-card-control.ts
+++ src/components/ui/weather-card-control/use-weather-card-control.ts
@@ -1,6 +1,8 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
+import axios from 'axios';
 import { CardsContext } from '../../complex/weather/cards-context';
+import { api } from '../../../const';
 
 const useWeatherCardControl = () => {
   const [isActive, setIsActive] = useState(false);
@@ -12,8 +14,22 @@ const useWeatherCardControl = () => {
   };
 
   const handleOnSubmit = ({ values }: any) => {
-    setCities([...cities, values.search]);
-    setIsActive(false);
+    useEffect(() => {
+      const searchWeather = async () => {
+        try {
+          await axios.get(
+            `${api.base}weather?q=${cities},ua&units=metric&APPID=${api.key}`
+          );
+          setCities([...cities, values.search]);
+          setIsActive(false);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      searchWeather().catch((error) => {
+        console.error(error);
+      });
+    }, []);
   };
 
   return {
